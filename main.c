cefcc8930390794a3a3c4961d70eef22998a28aa
Sujal-y <sujalyasho2006@gmail.com>
2024-10-15 22:01:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -14,51 +14,50 @@ printf("\t7|8|9\n");
 }
 
 void outputarray(){//displays array
-int i=0,j=0;
+    int i=0,j=0;
     printf("\t");
     while (i<3)
-    {
-    while(j== 0||j==1){
-    printf("%c|",r1[j]);
-    j++;
-    i++;
-    }
-    printf("%c",r1[i]);
-    i+=1;
-    }
+    {   
+        while(j== 0||j==1){
+        printf("%c|",r1[j]);
+        j++;i++;
+        }
+        printf("%c",r1[i]);
+        i+=1;
+        }
 
-    printf("\n\t");
-    i=0; j=0;
+        printf("\n\t");
+        i=0; j=0;
 
-    while (i<3)
-    {
-    while(j== 0||j==1)
-    {
-    printf("%c|",r2[j]);
-    j++; i++;
-    }
-    printf("%c",r2[i]);
-    i+=1;
-    }
+        while (i<3)
+        {
+        while(j== 0||j==1)
+        {
+        printf("%c|",r2[j]);
+        j++; i++;
+        }
+        printf("%c",r2[i]);
+        i+=1;
+        }
 
-    printf("\n\t");
-    i=0; j=0;
-    while (i<3)
-    {
-    while(j== 0||j==1)
-    {
-    printf("%c|",r3[j]);
-    j++; i++;
-    }
-    printf("%c",r3[i]);
-    i+=1;
-    }
-    printf("\n");
+        printf("\n\t");
+        i=0; j=0;
+        while (i<3)
+        {
+        while(j== 0||j==1)
+        {
+        printf("%c|",r3[j]);
+        j++; i++;
+        }
+        printf("%c",r3[i]);
+        i+=1;
+        }
+        printf("\n");
 }
 
 void switchPlayer() {//switches the current player
-if (current_player == 'X') current_player = 'O';
-else current_player = 'X';
+    if (current_player == 'X') current_player = 'O';
+    else current_player = 'X';
 }
 
 int checkTie() { //checks tie
@@ -66,126 +65,125 @@ int checkTie() { //checks tie
     else return 1;
 }
 
-int checkwin(){//checks weather match is finished or not
-//just hardcoded shit
-if (r1[0]=='X' && r1[1]=='X' && r1[2]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r2[0]=='X' && r2[1]=='X' && r2[2]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r3[0]=='X' && r3[1]=='X' && r3[2]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[0]=='X' && r2[0]=='X' && r3[0]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[1]=='X' && r2[1]=='X' && r3[1]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[2]=='X' && r2[2]=='X' && r3[2]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[0]=='X' && r2[1]=='X' && r3[2]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[2]=='X' && r2[1]=='X' && r3[0]=='X')
-{
-printf("X won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[0]=='O' && r1[1]=='O' && r1[2]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r2[0]=='O' && r2[1]=='O' && r2[2]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r3[0]=='O' && r3[1]=='O' && r3[2]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[0]=='O' && r2[0]=='O' && r3[0]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[1]=='O' && r2[1]=='O' && r3[1]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[2]=='O' && r2[2]=='O' && r3[2]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[0]=='O' && r2[1]=='O' && r3[2]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else if (r1[2]=='O' && r2[1]=='O' && r3[0]=='O')
-{
-printf("O won the game!!\n"); return 0;game_status =0;
-}
-else //if match not over it continues and changes current player
-{
-switchPlayer();
-return 1;
-}
+int checkwin(){//checks weather match is finished or not ,just hardcoded shit
+    if (r1[0]=='X' && r1[1]=='X' && r1[2]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r2[0]=='X' && r2[1]=='X' && r2[2]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r3[0]=='X' && r3[1]=='X' && r3[2]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[0]=='X' && r2[0]=='X' && r3[0]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[1]=='X' && r2[1]=='X' && r3[1]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[2]=='X' && r2[2]=='X' && r3[2]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[0]=='X' && r2[1]=='X' && r3[2]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[2]=='X' && r2[1]=='X' && r3[0]=='X')
+    {
+    printf("X won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[0]=='O' && r1[1]=='O' && r1[2]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r2[0]=='O' && r2[1]=='O' && r2[2]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r3[0]=='O' && r3[1]=='O' && r3[2]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[0]=='O' && r2[0]=='O' && r3[0]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[1]=='O' && r2[1]=='O' && r3[1]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[2]=='O' && r2[2]=='O' && r3[2]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[0]=='O' && r2[1]=='O' && r3[2]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else if (r1[2]=='O' && r2[1]=='O' && r3[0]=='O')
+    {
+    printf("O won the game!!\n"); return 0;game_status =0;
+    }
+    else //if match not over it continues and changes current player
+    {
+    switchPlayer();
+    return 1;
+    }
 }
 
 int makeMove(){//new play
-int input;
-reenter:
-printf("%c where do you want to place it? \n",current_player);
-scanf("%d",&input);
-if (input>0 && input<=9)
-{
-if (input==1 && r1[0]!=current_player && r1[0]!='O')
-{
-r1[0]=current_player;return 1;
-}
-else if (input==2 && r1[1]!='X' && r1[1]!='O')
-{
-r1[1]=current_player;return 1;
-}
-else if (input==3 && r1[2]!='X' && r1[2]!='O')
-{
-r1[2]=current_player;return 1;
-}
-else if (input==4 && r2[0]!='X' && r2[0]!='O')
-{
-r2[0]=current_player;return 1;
-}
-else if (input==5 && r2[1]!='X' && r2[1]!='O')
-{
-r2[1]=current_player;return 1;
-}
-else if (input==6 && r2[2]!='X' && r2[2]!='O')
-{
-r2[2]=current_player;return 1;
-}
-else if (input==7 && r3[0]!='X' && r3[0]!='O')
-{
-r3[0]=current_player;return 1;
-}
-else if (input==8 && r3[1]!='X' && r3[1]!='O')
-{
-r3[1]=current_player;return 1;
-}
-else if (input==9 && r3[2]!='X' && r3[2]!='O')
-{
-r3[2]=current_player;return 1;
-}
-else{printf("\talready there, try again\n");count++;goto reenter;}
-}
-else printf("%c did not enter a valid value, GAME OVER...\n",current_player);
-return 0;
+    int input;
+    reenter:
+    printf("%c where do you want to place it? \n",current_player);
+    scanf("%d",&input);
+    if (input>0 && input<=9)
+    {
+    if (input==1 && r1[0]!=current_player && r1[0]!='O')
+    {
+    r1[0]=current_player;return 1;
+    }
+    else if (input==2 && r1[1]!='X' && r1[1]!='O')
+    {
+    r1[1]=current_player;return 1;
+    }
+    else if (input==3 && r1[2]!='X' && r1[2]!='O')
+    {
+    r1[2]=current_player;return 1;
+    }
+    else if (input==4 && r2[0]!='X' && r2[0]!='O')
+    {
+    r2[0]=current_player;return 1;
+    }
+    else if (input==5 && r2[1]!='X' && r2[1]!='O')
+    {
+    r2[1]=current_player;return 1;
+    }
+    else if (input==6 && r2[2]!='X' && r2[2]!='O')
+    {
+    r2[2]=current_player;return 1;
+    }
+    else if (input==7 && r3[0]!='X' && r3[0]!='O')
+    {
+    r3[0]=current_player;return 1;
+    }
+    else if (input==8 && r3[1]!='X' && r3[1]!='O')
+    {
+    r3[1]=current_player;return 1;
+    }
+    else if (input==9 && r3[2]!='X' && r3[2]!='O')
+    {
+    r3[2]=current_player;return 1;
+    }
+    else{printf("\talready there, try again\n");count++;goto reenter;}
+    }
+    else printf("%c did not enter a valid value, GAME OVER...\n",current_player);
+    return 0;
 
 }
 
